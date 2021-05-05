ee457cb7c5369e9b78c2c9242dac6a7aceba3d57
Nicolás Torres Vargas <natorres@miuandes.cl>
2021-05-05 11:10:11 -0400

    first commit

---
+++ lab04.js
@@ -0,0 +1,16 @@
+let count = 0;
+const a = 1;
+
+const add = (count,a) => count + a;
+
+const substract = (count,) => count - a;
+
+
+var n = console.log("Choose an option: \n");
+
+console.log("(+) (-)\n\n(q) for quit");
+
+
+
+
+
