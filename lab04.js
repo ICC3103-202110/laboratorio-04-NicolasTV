8dd4e222323b2e8b453a372a3cfd9d4334cdbbc9
Nicolás Torres Vargas <natorres@miuandes.cl>
2021-05-06 17:22:43 -0400

    paso 1

--- lab04.js
+++ lab04.js
@@ -1,21 +1,8 @@
-let count = 0;
-const a = 1;
+
+let counter = 0;
 const ps = require("prompt-sync");
 const prompt = ps();
 
-let n = prompt("\nChoose an option: \n\n(+)\n(-)\n(q) for quit\n");
-
-while(n !== "q"){
-    const counter = (count, a) => {
-        if (n == "+") {
-            return counter = count + a;
-        }
-        else if (n == "-") {
-            return counter = count - a;
-        }
-        else {
-            console.log("\nInvalid command\n");
-            break;
-        }
-    }
+function view(counter){
+    return counter;
 }
