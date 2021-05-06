77857a1c4abc3696c153527fd089dc7c32db43b2
Nicolás Torres Vargas <natorres@miuandes.cl>
2021-05-06 17:43:23 -0400

    paso 2

--- lab04.js
+++ lab04.js
@@ -1,8 +1,22 @@
 
 let counter = 0;
+const a = 1;
 const ps = require("prompt-sync");
 const prompt = ps();
 
-function view(counter){
+function view(counter) {
     return counter;
+}
+
+function update(msg, counter) {
+    let msg = prompt("\n(+)  (-)\n");
+    if(msg = "+"){
+        return (counter + a);
+    }
+    else if(msg = "-"){
+        return (counter - a);
+    }
+    else{
+        return counter;
+    }
 }
